c08ca286519556f355710af64569aac25cac8a18
Marlon Carvalho Mariano <marlon.cmariano93@gmail.com>
2023-12-09 22:29:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Exercicio-Configuration
+ Exercício da aula de Configuration na cadeira de PW2 do IFRS
---
+++ src/main/java/com/pw2/HelloPW2.java
@@ -0,0 +1,20 @@
+package com.pw2;
+
+import jakarta.ws.rs.*;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+
+@Path("/hello-world")
+public class HelloPW2 {
+
+    @ConfigProperty(name = "Ifrs.pw2")
+    String ifrs;
+
+    @GET
+    @Produces("text/plain")
+    //retornar o valor em application.yaml
+    public String hello(){
+        return ifrs;
+    }
+
+}
